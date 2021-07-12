c6c95d9f160f254e62ac42d1f96f44d90a59cf9a
Kim-EunKyu <rladmsrb96@naver.com>
2021-07-12 23:25:24 +0900

    지뢰찾기 칸 구현

---
+++ src/App.jsx
@@ -0,0 +1,154 @@
+import React, { createContext, useMemo, useReducer } from 'react';
+import Form from './components/Form/Form';
+import Table from './components/Table';
+import "./App.css";
+
+export const CODE = {
+  MINE: -7,
+  NORMAL: -1,
+  QUESTION: -2,
+  FLAG: -3,
+  QUESTION_MINE: -4,
+  FLAG_MINE: -5,
+  CLICKED_MINE: -6,
+  OPENED: 0,
+}
+
+export const TableContext = createContext({
+  tableData: [],
+  halted: true,
+  dispatch: () => { }
+});
+
+const initialState = {
+  tableData: [],
+  timer: 0,
+  result: '',
+  halted: false,
+}
+
+const plantMine = (row, cell, mine) => {
+  console.log(row, cell, mine);
+  const candidate = Array(row * cell).fill().map((arr, i) => {
+    return i;
+  })
+  const shuffle = [];
+  while (candidate.length > row * cell - mine) {
+    const chosen = candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0];
+    shuffle.push(chosen);
+  }
+  const data = []
+  for (let i = 0; i < row; i++) {
+    const rowData = [];
+    data.push(rowData)
+    for (let j = 0; j < cell; j++) {
+      rowData.push(CODE.NORMAL)
+    }
+  }
+
+  for (let k = 0; k < shuffle.length; k++) {
+    const ver = Math.floor(shuffle[k] / cell);
+    const hor = shuffle[k] % cell;
+    data[ver][hor] = CODE.MINE;
+  }
+
+  console.log(data)
+  return data;
+
+}
+
+export const START_GAME = 'START_GAME'
+export const OPEN_CELL = 'OPEN_CELL'
+export const CLICK_MINE = 'CLICK_MINE'
+export const FLAG_CELL = 'FLAG_CELL'
+export const QUESTION_CELL = 'QUESTION_CELL'
+export const NORMALIZE_CELL = 'NORMALIZE_CELL'
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case START_GAME:
+      return {
+        ...state,
+        tableData: plantMine(action.row, action.cell, action.mine),
+        halted: false,
+      }
+    case OPEN_CELL: {
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+      tableData[action.row][action.cell] = CODE.OPENED;
+      return {
+        ...state,
+        tableData,
+      }
+    }
+    case CLICK_MINE: {
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+      tableData[action.row][action.cell] = CODE.CLICKED_MINE;
+      return {
+        ...state,
+        tableData,
+        halted: true,
+      }
+    }
+    case FLAG_CELL: {
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+      if (tableData[action.row][action.cell] === CODE.MINE) {
+        tableData[action.row][action.cell] = CODE.FLAG_MINE;
+      } else {
+        tableData[action.row][action.cell] = CODE.FLAG;
+      }
+      return {
+        ...state,
+        tableData,
+      }
+    }
+    case QUESTION_CELL: {
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+      if (tableData[action.row][action.cell] === CODE.FLAG_MINE) {
+        tableData[action.row][action.cell] = CODE.QUESTION_MINE;
+      } else {
+        tableData[action.row][action.cell] = CODE.QUESTION;
+      }
+      return {
+        ...state,
+        tableData,
+      }
+    }
+    case NORMALIZE_CELL: {
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+      if (tableData[action.row][action.cell] === CODE.QUESTION_MINE) {
+        tableData[action.row][action.cell] = CODE.MINE;
+      } else {
+        tableData[action.row][action.cell] = CODE.NORMAL;
+      }
+      return {
+        ...state,
+        tableData,
+      }
+    }
+    default:
+      return state;
+  }
+}
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { tableData, halted, timer, result } = state;
+  //Provider로 넘길때 이렇게 캐싱해 놓지 않으면 매번 자식 컴포넌트들이 리렌더링 되므로 성능 최적화를 위해 useMemo를 사용해준다.
+  const value = useMemo(() => ({ tableData, halted, dispatch }), [tableData, halted])
+
+  return (
+    <TableContext.Provider value={value}>
+      <Form />
+      <div>{timer}</div>
+      <Table />
+      <div>{result}</div>
+    </TableContext.Provider>
+  );
+};
+
+export default App;
